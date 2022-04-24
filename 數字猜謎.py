2ad71d4567b731084da02beef567ac2596ec5181
Cheryl Hsieh <ch84y1213y@gmail.com>
2022-04-24 12:06:50 +0800

    new commit

--- 數字猜謎.py
+++ 數字猜謎.py
@@ -1,17 +1,21 @@
-#產生一個隨機整數1~100
-#讓使用者重複輸入數字去猜()
-#猜對的話 印出"終於猜對了!"
-#猜錯的話 要告訴他 比答案大/小
 import random
+start = input('請決定隨機數字範圍開始值')
+end = input('請決定隨機數字範圍結束值')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100) #randint為函式
+r = random.randint(start, end) #randint為函式
+count = 0  
 while True: #無線循迴
+	count = count + 1 #快寫法可以寫成count += 1
 	num = input('請猜數字: ')
 	num = int(num) #型別轉換
 	if num == r:
 		print('你猜中了!')
+		print('這是你猜的第', count, '次')
 		break
 	elif num > r:
 		print('比答案大')
 	elif num < r:
 	    print('比答案小')	
+	print('這是你猜的第', count, '次') 
